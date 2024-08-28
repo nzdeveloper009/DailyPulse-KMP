64a4872978c9ea25c9ff3d716071e8e959c90f43
Syed Nokhaiz Al Hassan <nzdeveloper009@gmail.com>
2024-08-28 16:18:35 +0500

    init

---
+++ shared/src/commonMain/kotlin/com/nokhaiz/kmp/dailypulse/Platform.kt
@@ -0,0 +1,10 @@
+package com.nokhaiz.kmp.dailypulse
+
+expect class Platform {
+    val osName: String
+    val osVersion: String
+    val deviceModel: String
+    val density: Int
+
+    fun logSystemInfo()
+}
---
+++ shared/src/iosMain/kotlin/com/nokhaiz/kmp/dailypulse/Platform.ios.kt
@@ -0,0 +1,22 @@
+package com.nokhaiz.kmp.dailypulse
+
+import platform.UIKit.UIDevice
+import platform.UIKit.UIScreen
+
+actual class Platform {
+    actual val osName: String
+        get() = UIDevice.currentDevice.systemName
+    actual val osVersion: String
+        get() = UIDevice.currentDevice.systemVersion
+    actual val deviceModel: String
+        get() = UIDevice.currentDevice.model
+    actual val density: Int
+        get() = UIScreen.mainScreen.scale.toInt()
+
+    actual fun logSystemInfo() {
+        NSLog(
+            ("OS Name: $osName OS Version: $osVersion Device Model: $deviceModel Density: $density")
+        )
+    }
+
+}
